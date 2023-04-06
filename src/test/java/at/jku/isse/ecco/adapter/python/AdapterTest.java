acf81775bd4d516fb7cee0adbd41afd43e98dc09
Matthias Preuner <matthias.preuner@gmail.com>
2023-04-06 11:03:50 +0200

    replaced normal tests with JUnit-tests

--- src/test/java/at/jku/isse/ecco/adapter/python/AdapterTest.java
+++ src/test/java/at/jku/isse/ecco/adapter/python/AdapterTest.java
@@ -2,8 +2,13 @@ package at.jku.isse.ecco.adapter.python;
 
 import at.jku.isse.ecco.storage.mem.dao.MemEntityFactory;
 import at.jku.isse.ecco.tree.Node;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.testng.annotations.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,26 +21,101 @@ import java.util.stream.Stream;
 
 import static at.jku.isse.ecco.adapter.python.AdapterTestUtil.*;
 
+
 public class AdapterTest {
 
-    public static void main(String[] args) {
+    static Path readPath;
+    static Path writePath;
 
-        final String python = ".py";
-        final String jupyter = "ipynb";
-        final String json = "json";
+    final static String python = ".py";
+    final static String jupyter = "ipynb";
+    final static String json = "json";
+
+    static PythonReader reader;
+    static PythonWriter writer;
+
+    @BeforeAll
+    static void start() {
+        reader = new PythonReader(new MemEntityFactory());
+        writer = new PythonWriter();
+
+        Path cwd = Paths.get("").toAbsolutePath();
+
+        readPath = cwd.resolve("src/test/resources/read/").toAbsolutePath();
+        writePath = cwd.resolve("src/test/resources/write/").toAbsolutePath();
+
+        deleteDir(writePath);
+    }
+
+    @AfterAll
+    static void calculateMetricsForWholeTestSet() {
+        reader = null;
+        writer = null;
+    }
+
+    @Test(groups = {"test"})
+    @ParameterizedTest
+    @MethodSource("filesPython")
+    void testPython(final Path path) {
+        testFiles(path);
+    }
+
+    @Test(groups = {"test"})
+    @ParameterizedTest
+    @MethodSource("filesJupyter")
+    void testJupyter(final Path path) {
+        testFiles(path);
+    }
+
+    @Test(groups = {"test"})
+    @ParameterizedTest
+    @MethodSource("filesJson")
+    void testJson(final Path path) {
+        testFiles(path);
+    }
+
+
+    void testFiles(final Path path) {
+
+        Set<Node.Op> readNodes = reader.read(readPath, new Path[]{path});
+        Set<Node> writeNodes = new HashSet<>(readNodes);
+        writer.write(writePath, writeNodes);
+
+        Path outputPath = readPath.relativize(path);
+        outputPath = writePath.resolve(outputPath);
+
+        boolean identical = false;
+        if (path.toString().endsWith(python)) {
+            identical = comparePythonFiles(path, outputPath);
+        } else if (path.toString().endsWith(json)) {
+            identical = compareJsonFiles(path, outputPath);
+        } else if (path.toString().endsWith(jupyter)) {
+            identical = compareJupyterFiles(path, outputPath);
+        } else {
+            System.out.println("Unknown File extension");
+        }
+
+        Assertions.assertTrue(identical);
+    }
+
+    public static Path[] filesPython() {
+        return files(python);
+    }
+
+    public static Path[] filesJupyter() {
+        return files(jupyter);
+    }
+
+    public static Path[] filesJson() {
+        return files(json);
+    }
+
+    public static Path[] files(String type) {
+        List<Path> files;
 
         //final String[] excludedFolders = new String[]{"yolov5-master", "ass1", "ass2"};
         final String[] excludedFolders = new String[] {};
 
-        File readFolder = new File("adapter/python/src/test/resources/read/");
-        File writeFolder = new File("adapter/python/src/test/resources/write/");
-
-        Path readPath = Paths.get(readFolder.getAbsolutePath());
-        Path writePath = Paths.get(writeFolder.getAbsolutePath());
-
-        deleteDir(writePath);
-
-        List<Path> files;
         try (Stream<Path> s1 = Files.walk(readPath);
              Stream<Path> s2 = Files.walk(readPath)) {
 
@@ -51,52 +131,12 @@ public class AdapterTest {
 
             files = s2.filter(f -> !Files.isDirectory(f))
                     .filter(f -> Arrays.stream(excludedFolders).noneMatch(s -> f.toAbsolutePath().toString().contains(s)))
-                    .filter(f -> f.getFileName().toString().endsWith(python) || f.getFileName().toString().endsWith(jupyter) || f.getFileName().toString().endsWith(json)).toList();
+                    .filter(f -> f.getFileName().toString().endsWith(type)).toList();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        Path[] inputFiles = files.toArray(new Path[0]);
-
-        System.out.println("reading ...");
-        PythonReader reader = new PythonReader(new MemEntityFactory());
-        Set<Node.Op> readNodes = reader.read(Paths.get(readFolder.getAbsolutePath()), inputFiles);
-        System.out.println("reading ... successful");
-
-        System.out.println("writing ...");
-        PythonWriter writer = new PythonWriter();
-        Set<Node> writeNodes = new HashSet<>(readNodes);
-        writer.write(Paths.get(writeFolder.getAbsolutePath()), writeNodes);
-        System.out.println("writing ... successful");
-
-        // compare all Files
-        System.out.println("checking for equality ...");
-        boolean allIdentical = true;
-        for (Path inputPath : files) {
-
-            Path outputPath = readPath.relativize(inputPath);
-            outputPath = writePath.resolve(outputPath);
-
-            boolean identical = false;
-            if (inputPath.toString().endsWith(python)) {
-                identical = comparePythonFiles(inputPath, outputPath);
-            } else if (inputPath.toString().endsWith(json)) {
-                identical = compareJsonFiles(inputPath, outputPath);
-            } else if (inputPath.toString().endsWith(jupyter)) {
-                identical = compareJupyterFiles(inputPath, outputPath);
-            } else {
-                System.out.println("Unknown File extension");
-            }
-
-            if (!identical) {
-                System.out.println("Writing of " + outputPath + " NOT identical");
-                allIdentical = false;
-            }
-
-        }
-        if (allIdentical) {
-            System.out.println("checking for equality ... successful - all files are identical");
-        }
-
+        return files.toArray(new Path[0]);
     }
+
 }
